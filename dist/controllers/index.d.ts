9d7377946b995556c5e6eb179b531c821f9f01a2
Aldrin Bryan Degracia <abdegracia@YONDU105513.local>
2025-12-07 19:04:05 +0800

    Update dist file

--- dist/controllers/index.d.ts
+++
@@ -1,6 +0,0 @@
-import EmployeesController from './employees';
-export { EmployeesController };
-declare const _default: {
-    EmployeesController: typeof EmployeesController;
-};
-export default _default;
--- dist/controllers/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EmployeesController = void 0;
-const employees_1 = __importDefault(require("./employees"));
-exports.EmployeesController = employees_1.default;
-exports.default = { EmployeesController: employees_1.default };
